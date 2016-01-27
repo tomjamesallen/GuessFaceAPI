a7e7a4c88bae699070545121a93d73af37286f7b
Tom Allen <tomjamesallenweb@gmail.com>
2016-01-27 13:02:53 +0000

    Enabling cross-origin requests on dev server.

--- tasks/dev-server.js
+++ tasks/dev-server.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var path = require('path');
 var connect = require('gulp-connect');
+var cors = require('cors');
+
+var corsOptions = {
+  // origin: '*'
+};
 
 var portNumber = 8777;
 
 gulp.task('dev-server', function () {
   connect.server({
     root: 'dist/api',
-    port: portNumber
+    port: portNumber,
+    middleware: function() {
+      return [cors(corsOptions)];
+    }
   });
 });
