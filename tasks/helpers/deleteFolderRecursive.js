cd7e88091b2f025063c95760723aaf73b40de80e
Tom Allen <tomjamesallenweb@gmail.com>
2018-01-01 18:18:52 +0000

    Making deleteFolderRecursive safer

--- tasks/helpers/deleteFolderRecursive.js
+++ tasks/helpers/deleteFolderRecursive.js
@@ -2,6 +2,11 @@ var fs = require('fs');
 var path = require('path');
 
 var deleteFolderRecursive = function(path) {
+  if (path.indexOf("./") !== 0) {
+    throw new Error("unsafe path passed to `deleteFolderRecursive`. Exiting");
+    return
+  }
+  return;
   if( fs.existsSync(path) ) {
     fs.readdirSync(path).forEach(function(file,index){
       var curPath = path + "/" + file;
